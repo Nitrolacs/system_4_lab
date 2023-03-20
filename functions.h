8ca07f6200f38631149fd7f49849e83ac6e8af00
Terentev Nikita <nitro-2003@mail.ru>
2023-03-20 22:17:47 +0700

    Changed the structure of the project

---
+++ functions.h
@@ -0,0 +1,20 @@
+//
+// Created by stud on 20.03.23.
+//
+
+#ifndef INC_4_LAB_FUNCTIONS_H
+#define INC_4_LAB_FUNCTIONS_H
+
+void add_record();
+
+void modify_record();
+
+void delete_record();
+
+void read_record();
+
+void print_all_records();
+
+
+
+#endif //INC_4_LAB_FUNCTIONS_H
---
+++ interface.h
@@ -0,0 +1,10 @@
+//
+// Created by stud on 20.03.23.
+//
+
+#ifndef INC_4_LAB_INTERFACE_H
+#define INC_4_LAB_INTERFACE_H
+
+void PrintMenu();
+
+#endif //INC_4_LAB_INTERFACE_H
